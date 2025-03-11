6ec9101a07acb31932536c9e71273d88b573ac90
mario <mario2004mario16@hotmail.com>
2025-03-11 20:13:18 +0000

    Refactor of counter js logic

--- software.ulpgc.timac/js/scripts.js
+++ software.ulpgc.timac/js/scripts.js
@@ -1,24 +1,27 @@
 document.addEventListener('DOMContentLoaded', function () {
-
-    // ESTO SE DEBERÍA DE HACER CON UN OBSERVER, NO CON UN TIMEOUT
-
-    setTimeout(() => {
+    const observer = new MutationObserver(() => {
         const decreaseButton = document.getElementById("decrease");
         const increaseButton = document.getElementById("increase");
         const counterElement = document.getElementById("counter");
 
-        let count = 0;
-
-        increaseButton.addEventListener("click", function () {
-            count++;
-            counterElement.textContent = count;
-        });
-
-        decreaseButton.addEventListener("click", function () {
-            if (count > 0){
-                count--;
-                counterElement.textContent = count;
-            }
-        });
-    }, 500); // Espera medio segundo para asegurarte de que se cargó el contenido
+        initCounter(decreaseButton, increaseButton, counterElement);
+    });
+    observer.observe(document.body, {childList: true, subtree: true});
 });
+
+function initCounter(decreaseButton, increaseButton, counterElement) {
+
+    let count = parseInt(counterElement.textContent);
+
+    increaseButton.addEventListener("click", function () {
+        count++;
+        counterElement.textContent = count;
+    });
+
+    decreaseButton.addEventListener("click", function () {
+        if (count > 0){
+            count--;
+            counterElement.textContent = count;
+        }
+    });
+}
