ad3b6168364f3e890ef2f8886e6789d494bf4cfa
Usuario <juan.rodriguez215@alu.ulpgc.es>
2025-03-17 16:06:14 +0000

    Functional version.

--- software.ulpgc.timac/js/xlu-include-file.js
+++ software.ulpgc.timac/js/xlu-include-file.js
@@ -1,32 +1,22 @@
 async function xLuIncludeFile() {
-    let elements = document.getElementsByTagName("*");
-    let processed = false;
+    let elements = document.querySelectorAll("[xlu-include-file]");
 
-    for (let i = 0; i < elements.length; i++) {
-        if (elements[i].getAttribute("xlu-include-file")) {
-            let clone = elements[i].cloneNode(false);
-            let file = elements[i].getAttribute("xlu-include-file");
-
-            try {
-                let response = await fetch(file);
-                if (response.ok) {
-                    let content = await response.text();
-                    clone.removeAttribute("xlu-include-file");
-                    clone.innerHTML = content;
-                    elements[i].parentNode.replaceChild(clone, elements[i]);
-                    processed = true;
-                } else {
-                    console.error(`Error loading file ${file}: HTTP ${response.status}`);
-                }
-            } catch (error) {
-                console.error("Error fetching file:", error);
+    for (let element of elements) {
+        let file = element.getAttribute("xlu-include-file");
+        try {
+            let response = await fetch(file);
+            if (response.ok) {
+                let content = await response.text();
+                let clone = element.cloneNode(false);
+                clone.removeAttribute("xlu-include-file");
+                clone.innerHTML = content;
+                element.parentNode.replaceChild(clone, element);
+            } else {
+                console.error(`Error loading file ${file}: HTTP ${response.status}`);
             }
+        } catch (error) {
+            console.error("Error fetching file:", error);
         }
     }
-
-    if (processed) {
-        xLuIncludeFile(); // Recursivo solo si se procesó algo
-    }
 }
-
-xLuIncludeFile();
+xLuIncludeFile().then(r => loadProduct());
