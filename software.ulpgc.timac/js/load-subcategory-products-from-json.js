0b4e14d041c7edeaaa210d046b0a35cb43801eba
IanSamuelTrujilloGil <iansamueltrujillo@@gmail.com>
2025-03-18 19:08:45 +0000

    Minor changes.

--- software.ulpgc.timac/js/load-subcategory-products-from-json.js
+++ software.ulpgc.timac/js/load-subcategory-products-from-json.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', async () => {
+document.addEventListener('DOMContentLoaded',  () => {
     function loadProducts() {
 
         const productContainer = document.querySelector('.product-display-product-container');
