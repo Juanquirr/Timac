422537b01fb8018e8f203187fdf43c9b58636703
Juanquirr <juanquirr579@gmail.com>
2025-03-19 11:33:11 +0000

    Changes

--- software.ulpgc.timac/js/basket.js
+++ software.ulpgc.timac/js/basket.js
@@ -1,73 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     function loadDetailedProduct() {
-        fetch('../json/detailed-product.json')
+        fetch('../json/basket.json')
             .then(response => response.json())
             .then(product => {
-                const imageContainer = document.querySelector('.top-box-detailed-product-left');
-                const img = document.createElement('img');
-                img.src = product.image;
-                img.alt = product.alt;
-                imageContainer.appendChild(img);
-
-                const informationContainer = document.querySelector('.information-detailed-product');
-                const information = document.createElement('div');
-                information.innerHTML = `
-                    <h1 class="body-title font-base">${product.title}</h1>
-                    <h2 class="big-price font-base">${product.price}</h2>
-                    <p class="body-text font-base">
-                        Subcategory: ${product.subcategory}<br>
-                        Brand: ${product.brand}<br>
-                        Color: ${product.color}<br>
-                        Specific attribute 1: ${product.specific_attribute_1}<br>
-                        Specific attribute 2: ${product.specific_attribute_2}
-                    </p>
-                    <div class="availability">
-                        <div class="in-store">
-                            <img src="../assets/star.png" alt="Star"/>
-                            <p class="body-text font-base">${product.availability['in-store'] ? 'AVAILABLE IN STORE' : 'NOT AVAILABLE IN STORE'}</p>
-                        </div>
-                        <div class="in-store">
-                            <img src="../assets/arrow.png" alt="Arrow"/>
-                            <p class="body-text font-base">${product.availability.delivery ? 'AVAILABLE FOR DELIVERY' : 'NOT AVAILABLE FOR DELIVERY'}</p>
-                        </div>
-                    </div>
-                `;
-                informationContainer.appendChild(information);
-
-                const decreaseBtn = document.getElementById('decrease');
-                const increaseBtn = document.getElementById('increase');
-                const counterSpan = document.getElementById('counter');
-                let counter = 0;
-                decreaseBtn.addEventListener('click', () => {
-                    if (counter > 0) {
-                        counter--;
-                        counterSpan.textContent = counter;
-                    }
-                });
-
-                increaseBtn.addEventListener('click', () => {
-                    counter++;
-                    counterSpan.textContent = counter;
-                    localStorage.setItem("hammer", counter)
-                });
-
-                const addBasketBtn = document.getElementById('add-basket-button');
-
-                addBasketBtn.addEventListener('click', (_) => {
-                    console.log(`Added ${counter} ${product.title} to basket`);
-                });
-
-                const descriptionContainer = document.querySelector('.bottom-box-detailed-product');
-                descriptionContainer.innerHTML = `
-                    <h2 class="body-title font-base">About this item</h2>
-                    <p class="body-text font-base">${product.description.join('<br>')}</p>
-                `;
 
             })
             .catch(error => {
                 console.error('Error loading product:', error);
-                document.getElementById('detailed-product').innerHTML = '<p>Error loading product json</p>';
+                document.getElementById('   ').innerHTML = '<p>Error loading product json</p>';
             });
     }
 
@@ -86,5 +27,5 @@ document.addEventListener("DOMContentLoaded", () => {
         observer.observe(document.body, { childList: true, subtree: true });
     }
 
-    waitForElement(".top-box-detailed-product-left", loadDetailedProduct);
+    waitForElement(".", loadDetailedProduct);
 });
