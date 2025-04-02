04a5eab05bec4e23805c0a34e1d3790dcc4b3639
Juanquirr <juanquirr579@gmail.com>
2025-04-02 18:27:40 +0100

    Properly swapped to Angular project.

--- software.ulpgc.timac/js/load-new-at-timac-products-from-json.js
+++
@@ -1,5 +0,0 @@
-import { waitForElement, loadHomePageProductsFromJSON } from './utils.js';
-
-document.addEventListener('DOMContentLoaded',  () => {
-    waitForElement("#product-section-new-at-timac", ()=> loadHomePageProductsFromJSON("../json/products.json","#product-section-new-at-timac", "new"));
-});
--- software.ulpgc.timac/js/load-offers-products-from-json.js
+++
@@ -1,5 +0,0 @@
-import { waitForElement, loadHomePageProductsFromJSON } from './utils.js';
-
-document.addEventListener('DOMContentLoaded',  () => {
-    waitForElement("#product-section-offers", ()=> loadHomePageProductsFromJSON("../json/products.json","#product-section-offers", "on_sale"));
-});
