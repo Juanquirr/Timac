6581dfddcfa80d05c4237338f8554e2af825d892
Juanquirr <juanquirr579@gmail.com>
2025-04-07 15:30:43 +0100

    Header component buttons adjustment.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   templateUrl: './header.component.html',
+  imports: [
+    NgIf
+
+  ],
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  areLoginButtonsDisplaying: boolean = false;
+
+  toggleLoginButtons(): void {
+    this.areLoginButtonsDisplaying = !this.areLoginButtonsDisplaying;
+  }
+
+}
