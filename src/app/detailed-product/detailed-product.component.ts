aeb564bb397d91650e60fd4caf5b2b1b623f9a8d
Mario <mario2004mario16@hotmail.com>
2025-04-13 20:41:31 +0100

    Prevent adding non existent products to basket and added error handling.

--- src/app/detailed-product/detailed-product.component.ts
+++ src/app/detailed-product/detailed-product.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {HttpClient} from '@angular/common/http';
-import {NgForOf} from '@angular/common';
+import {NgForOf, NgIf} from '@angular/common';
 
 interface Product {
   id: string;
@@ -21,6 +21,10 @@ interface Product {
   description: string[];
 }
 
+interface ProductList {
+  products: Product[];
+}
+
 interface BasketItem {
   id: string;
   quantity: number;
@@ -30,26 +34,40 @@ interface BasketItem {
   selector: 'app-detailed-product',
   standalone: true,
   imports: [
-    NgForOf
+    NgForOf,
+    NgIf
   ],
   templateUrl: './detailed-product.component.html',
   styleUrl: './detailed-product.component.css'
 })
 export class DetailedProductComponent implements OnInit {
-  product: Product = {} as Product;
+  product: Product | undefined;
   productId: string = '';
   counter: number = 1;
 
   constructor(private activatedRoute: ActivatedRoute, private http: HttpClient, private router: Router) {}
+
   ngOnInit() {
     this.productId = this.activatedRoute.snapshot.paramMap.get('id')!;
-    this.http.get<any>('../assets/products.json').subscribe(data => {
-      this.product = data.products.find((p: Product) => p.id == this.productId);
+
+    this.http.get<ProductList>('../assets/products.json').subscribe({
+      next: data => {
+        const foundProduct = data.products.find((p: Product) => p.id === this.productId);
+        if (foundProduct) {
+          this.product = foundProduct;
+        } else {
+          console.error('Product not found');
+          this.router.navigate(['/']).catch(error => console.error('Navigation error', error));
+        }
+      },
+      error: (error) => {
+        console.error('Error loading JSON', error);
+      }
     });
   }
 
   addToBasket() {
-    if (this.counter > 0) {
+    if (this.counter > 0 && this.product) {
       let basket: BasketItem[] = JSON.parse(localStorage.getItem('basket') || '[]');
       const existingProductIndex = basket.findIndex(item => item.id == this.productId);
 
@@ -58,11 +76,13 @@ export class DetailedProductComponent implements OnInit {
       } else{
         basket.push({id: this.productId, quantity: this.counter});
       }
+
       localStorage.setItem('basket', JSON.stringify(basket));
       console.log(`Added ${this.counter} ${this.product.name} to Basket`);
-      this.router.navigate(['/basket']);
+      this.router.navigate(['/basket']).catch(error => console.error('Navigation error', error));
     } else {
       console.log('Please select a quantity greater than 0');
+      alert('Please select a quantity greater than 0');
     }
   }
 
