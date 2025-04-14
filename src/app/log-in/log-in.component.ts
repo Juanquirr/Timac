7e16d3566023a80dbe9ef24380369ccba87adc3a
Mario <mario2004mario16@hotmail.com>
2025-04-14 20:32:28 +0100

    Basic log-in form added

---
+++ src/app/log-in/log-in.component.ts
@@ -0,0 +1,38 @@
+import {Component, OnInit} from '@angular/core';
+import {RouterLink} from '@angular/router';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {NgIf} from '@angular/common';
+
+@Component({
+  selector: 'app-log-in',
+  imports: [
+    RouterLink,
+    ReactiveFormsModule,
+    NgIf
+  ],
+  templateUrl: './log-in.component.html',
+  styleUrl: './../sign-up/sign-up.component.css' // Usa el mismo que el sign-up
+})
+export class LogInComponent implements OnInit {
+  userForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(){
+    this.userForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  onSumbit(){
+    if(this.userForm.invalid){
+      console.log('This form is invalid');
+      return;
+    }
+
+    console.log('Valid form', this.userForm.value);
+    // AQUÍ SE MANEJARÁN LOS DATOS CON FIREBASE
+
+  }
+}
