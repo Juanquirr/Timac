454bcbb69a7c41ce2c0ded205aa9676fe8837cc6
Mario <mario2004mario16@hotmail.com>
2025-04-15 11:36:58 +0100

    Main-layout added and changed routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,18 +5,22 @@ import {DetailedProductComponent} from './detailed-product/detailed-product.comp
 import {AuthLayoutComponent} from './auth-layout/auth-layout.component';
 import {SignUpComponent} from './sign-up/sign-up.component';
 import {LogInComponent} from './log-in/log-in.component';
+import {MainLayoutComponent} from './main-layout/main-layout.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeSectionComponent },
-  { path: 'offers', component: HomeSectionComponent },
-  { path: 'building', component: HomeSectionComponent },
-  { path: 'heating', component: HomeSectionComponent },
-  { path: 'lighting', component: HomeSectionComponent },
-  { path: 'painting', component: HomeSectionComponent },
-  { path: 'tools', component: HomeSectionComponent },
-  { path: 'trending', component: HomeSectionComponent },
-  { path: 'basket', component: BasketComponent },
-  { path: 'product/:id', component: DetailedProductComponent },
+  { path: '', component: MainLayoutComponent, children: [
+      { path: '', component: HomeSectionComponent},
+      { path: 'offers', component: HomeSectionComponent },
+      { path: 'building', component: HomeSectionComponent },
+      { path: 'heating', component: HomeSectionComponent },
+      { path: 'lighting', component: HomeSectionComponent },
+      { path: 'painting', component: HomeSectionComponent },
+      { path: 'tools', component: HomeSectionComponent },
+      { path: 'trending', component: HomeSectionComponent },
+      { path: 'basket', component: BasketComponent },
+      { path: 'product/:id', component: DetailedProductComponent },
+    ]
+  },
   { path: '', component: AuthLayoutComponent, children: [
       {path: 'sign-up', component: SignUpComponent},
       {path: 'log-in', component: LogInComponent }
