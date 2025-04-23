e09f2f9574e0c397a72ddbe4740f8e9425182741
mario <mario2004mario16@hotmail.com>
2025-04-23 14:41:45 +0100

    Auth service

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import {doc, Firestore, setDoc} from '@angular/fire/firestore';
+import {Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut} from '@angular/fire/auth';
+import { UserCredential } from 'firebase/auth';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  constructor(private auth: Auth, private firestore: Firestore) {}
+
+  async register(email: string, password: string, additionalData: { name: string; phone: string; birthDate: string}): Promise<UserCredential> {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(this.auth, email, password);
+      const user = userCredential.user;
+
+      await setDoc(doc(this.firestore, 'users', user.uid), {
+        email: user.email,
+        name: additionalData.name,
+        phone: additionalData.phone,
+        birthDate: additionalData.birthDate,
+      });
+
+      return userCredential;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async login(email: string, password: string): Promise<UserCredential> {
+    try {
+      return await signInWithEmailAndPassword(this.auth, email, password)
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async logout(): Promise<void> {
+    try {
+      await signOut(this.auth);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  getCurrentUser() {
+    return this.auth.currentUser;
+  }
+}
