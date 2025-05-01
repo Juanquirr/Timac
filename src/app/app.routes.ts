f442c9dbabfbf752e03d280274a70f79afd4dd7f
Juanquirr <juanquirr579@gmail.com>
2025-05-01 21:34:10 +0100

    Refactor of the structure of files.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,15 @@
 import { Routes } from '@angular/router';
-import {BasketComponent} from './components/basket/basket.component';
-import {HomeSectionComponent} from './components/home-section/home-section.component';
-import {DetailedProductComponent} from './components/detailed-product/detailed-product.component';
-import {AuthLayoutComponent} from './components/auth-layout/auth-layout.component';
-import {SignUpComponent} from './components/sign-up/sign-up.component';
-import {LogInComponent} from './components/log-in/log-in.component';
-import {MainLayoutComponent} from './components/main-layout/main-layout.component';
-import {SubcategoryResultsComponent} from './components/subcategory-results/subcategory-results.component';
+import {BasketComponent} from './shared/components/basket/basket.component';
+import {HomeSectionComponent} from './shared/components/home-section/home-section.component';
+import {DetailedProductComponent} from './shared/components/detailed-product/detailed-product.component';
+import {AuthLayoutComponent} from './shared/components/auth-layout/auth-layout.component';
+import {SignUpComponent} from './shared/components/sign-up/sign-up.component';
+import {LogInComponent} from './shared/components/log-in/log-in.component';
+import {MainLayoutComponent} from './shared/components/main-layout/main-layout.component';
+import {SubcategoryResultsComponent} from './shared/components/subcategory-results/subcategory-results.component';
 import {
   SubcategoryOptionSelectorComponent
-} from './components/subcategory-option-selector/subcategory-option-selector.component';
+} from './shared/components/subcategory-option-selector/subcategory-option-selector.component';
 
 export const routes: Routes = [
   { path: '', component: MainLayoutComponent, children: [
---
+++ src/app/shared/components/header/header.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import {NgIf} from '@angular/common';
+import {RouterLink} from '@angular/router';
+import {AuthService} from '../../../core/services/auth.service';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  templateUrl: './header.component.html',
+  imports: [
+    NgIf,
+    RouterLink
+
+  ],
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+  areLoginButtonsDisplaying: boolean = false;
+
+  constructor(private authService: AuthService) {}
+
+  toggleLoginButtons(): void {
+    this.areLoginButtonsDisplaying = !this.areLoginButtonsDisplaying;
+  }
+
+  checkLoginStatus(){
+    return !!this.authService.getCurrentUser();
+  }
+
+  handleLogout() {
+    this.authService.logout().catch(error => console.error('Logout error: ', error));
+  }
+}
