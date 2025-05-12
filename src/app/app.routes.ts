fc6f9ce7b021eeeae6c3ae8351c3bfa5025b9ed5
Juanquirr <juanquirr579@gmail.com>
2025-05-12 16:25:32 +0100

    Pages created and global.scss updated.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,27 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    loadComponent: () => import('./pages/home/home.page').then((m) => m.HomePage),
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
+  },
+  {
+    path: 'detailed-product',
+    loadComponent: () => import('./pages/detailed-product/detailed-product.page').then( m => m.DetailedProductPage)
+  },
+  {
+    path: 'favorites',
+    loadComponent: () => import('./pages/favorites/favorites.page').then( m => m.FavoritesPage)
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./pages/register/register.page').then( m => m.RegisterPage)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
   },
 ];
