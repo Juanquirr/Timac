da7e2db48f9f711b41426481b48122750025dc8a
Juanquirr <juanquirr579@gmail.com>
2025-05-12 16:28:34 +0100

    Merge fix with main.

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import {HeaderComponent} from "../components/header/header.component";
-import {FooterComponent} from "../components/footer/footer.component";
+import {HeaderComponent} from "../../components/header/header.component";
+import {FooterComponent} from "../../components/footer/footer.component";
 
 @Component({
   selector: 'app-home',
