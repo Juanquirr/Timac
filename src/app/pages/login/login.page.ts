b29ded4210375386e18e08b15d59a16d3fc242f4
mario <mario2004mario16@hotmail.com>
2025-05-14 22:24:08 +0100

    Login and register implemented

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -2,7 +2,8 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 import {Subscription} from "rxjs";
-import {RouterLink} from "@angular/router";
+import {Router, RouterLink} from "@angular/router";
+import {AuthService} from "../../core/services/auth.service";
 
 @Component({
   selector: 'app-login',
@@ -17,11 +18,17 @@ export class LoginPage implements OnInit, OnDestroy {
   userSubscription!: Subscription;
   isLoading = false;
 
-  constructor(private formBuilder: FormBuilder,
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router
   ) {}
 
   ngOnInit(){
-    // COMPROBAR QUE NO HAY NADIE LOGEADO
+    this.userSubscription = this.authService.getAuthState().subscribe(user => {
+      this.currentUser = user
+      this.isLoading = false;
+    });
 
     this.userForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
@@ -29,12 +36,37 @@ export class LoginPage implements OnInit, OnDestroy {
     });
   }
 
-  onSubmit() {
-    if (this.userForm.invalid) {
+  onSubmit(){
+    if(this.userForm.invalid){
       console.log('This form is invalid');
       return;
     }
+
+    console.log('Valid form for user: ', this.userForm.value.email);
+
+    this.authService.login(this.userForm.value.email, this.userForm.value.password).then((userCredential) => {
+      if (userCredential.user) {
+        this.router.navigate(['/']).catch(error => console.error('Navigation error', error));
+      }
+    }).catch(error => {
+      console.error('Error during log-in:', error);
+      let errorMsg = 'Log-in error, please try again.';
+      const errorCode = error.code;
+      switch (errorCode){
+        case 'auth/invalid-credential':
+          errorMsg = 'Email or password incorrect.';
+          break;
+        case 'auth/too-many-requests':
+          errorMsg = 'Too many requests, try again later.';
+          break;
+        case 'auth/user-disabled':
+          errorMsg = 'This account is disabled.';
+          break;
+      }
+      this.userForm.setErrors({ loginError: errorMsg });
+    });
   }
+
   ngOnDestroy() {
     if(this.userSubscription){
       this.userSubscription.unsubscribe();
@@ -42,7 +74,6 @@ export class LoginPage implements OnInit, OnDestroy {
   }
 
   handleLogout() {
-    // LOGOUT CON AUTHSERVICE
+    this.authService.logout().catch(error => console.error('Logout error: ', error));
   }
-
 }
--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -2,7 +2,8 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 import {Subscription} from "rxjs";
-import {RouterLink} from "@angular/router";
+import {Router, RouterLink} from "@angular/router";
+import {AuthService} from "../../core/services/auth.service";
 
 @Component({
   selector: 'app-register',
@@ -19,10 +20,15 @@ export class RegisterPage implements OnInit, OnDestroy {
 
   constructor(
     private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
   ) {}
 
   ngOnInit() {
-    // AQUÍ VA LA COMPROBACIÓN DEL USUARIO
+    this.userSubscription = this.authService.getAuthState().subscribe(user => {
+      this.currentUser = user
+      this.isLoading = false;
+    });
 
     this.userForm = this.formBuilder.group({
       name: ['', [Validators.required, Validators.maxLength(32)]],
@@ -34,21 +40,43 @@ export class RegisterPage implements OnInit, OnDestroy {
     }, {validators: this.passwordMatchValidator});
   }
 
-  passwordMatchValidator(form: FormGroup){
-    if(form.get('password1')?.value === form.get('password2')?.value){
+  passwordMatchValidator(form: FormGroup) {
+    if(form.get('password1')?.value === form.get('password2')?.value) {
       return null;
     } else {
-      return {passwordMismatch: true};
+      return { passwordMismatch: true };
     }
   }
 
-  onSubmit(){
-    if(this.userForm.invalid){
+  onSubmit() {
+    if (this.userForm.invalid) {
       console.log('This form is invalid');
       return;
     }
 
-    // LOGICA DE REGISTER
+    this.authService.register(
+      this.userForm.value.email,
+      this.userForm.value.password1,
+      {name: this.userForm.value.name, phone: this.userForm.value.phone, birthDate: this.userForm.value.birthDate}
+    ).then(() => {
+      this.router.navigate(['/']).catch(error => console.error('Navigation error', error))
+    }).catch(error => {
+      console.error('Error during sign-in:', error);
+      let errorMsg = 'Sign-in error, please try again.';
+      const errorCode = error.code;
+      switch (errorCode) {
+        case 'auth/email-already-in-use':
+          errorMsg = 'Email already in use.';
+          break;
+        case 'auth/operation-not-allowed':
+          errorMsg = 'Operation not allowed, please log out first.';
+          break;
+        case 'auth/too-many-requests':
+          errorMsg = 'Too many requests, try again later.';
+          break;
+      }
+      this.userForm.setErrors({registrationError: errorMsg});
+    });
 
   }
 
@@ -58,8 +86,7 @@ export class RegisterPage implements OnInit, OnDestroy {
     }
   }
 
-  handleLogout(){
-    // LLAMAR AL AUTHSERVICE PARA LOGOUT
-    console.log('Logout');
+  handleLogout() {
+    this.authService.logout().catch(error => console.error('Logout error: ', error));
   }
 }
