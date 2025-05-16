98dac59ced4023670fc24d0c622a43381c5f55ad
mario <mario2004mario16@hotmail.com>
2025-05-16 10:24:13 +0100

    Fixed bug with login/unlogged state

--- src/app/pages/favorites/favorites.page.ts
+++ src/app/pages/favorites/favorites.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import {
@@ -12,37 +12,44 @@ import {BasketItem} from "../../core/model/basketItem.model";
 import {Product} from "../../core/model/product.model";
 import {FavoriteItemComponent} from '../../components/favorite-item/favorite-item.component';
 import {AuthService} from "../../core/services/auth.service";
-import {Router} from "@angular/router";
+import {Router, RouterLink} from "@angular/router";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-favorites',
   templateUrl: './favorites.page.html',
   styleUrls: ['./favorites.page.scss'],
   standalone: true,
-  imports: [CommonModule, FormsModule, IonCard, IonCardHeader, IonCardTitle, IonList, FavoriteItemComponent]
+  imports: [CommonModule, FormsModule, IonCard, IonCardHeader, IonCardTitle, IonList, FavoriteItemComponent, RouterLink]
 })
-export class FavoritesPage implements OnInit {
+export class FavoritesPage implements OnInit, OnDestroy {
   favoriteProducts: Map<number, Product> = new Map();
   favoriteIdOfUser: Map<number, number> = new Map();
   userProducts: BasketItem[] = [];
   totalItems: number = 0;
+  userUid: any = null;
+  userSubscription!: Subscription;
 
   constructor(private firebaseService: FirebaseService, private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
-    this.loadFavorites();
+    this.userSubscription = this.authService.getAuthState().subscribe(user => {
+      if (user){
+        this.userUid = user.uid
+        this.loadFavorites();
+      } else {
+        this.userUid = null;
+      }
+    });
   }
 
   loadFavorites(): void {
-    let userUid = this.authService.getCurrentUser()?.uid;
-
-    if (!userUid) {
-      alert('You are not logged in');
-      this.router.navigate(['/']);
+    if(!this.userUid) {
+      console.log('You are not logged in');
       return;
     }
 
-    this.firebaseService.getUserById(userUid).subscribe(userDoc => {
+    this.firebaseService.getUserById(this.userUid).subscribe(userDoc => {
       if (userDoc.basket) this.userProducts = userDoc.basket;
       this.userProducts.forEach(basketItem => {
         this.firebaseService.getProductByFieldId('products', Number(basketItem.id)).subscribe(products => {
@@ -66,5 +73,11 @@ export class FavoritesPage implements OnInit {
     });
     this.firebaseService.updateUserBasket(this.authService.getCurrentUser()!.uid, favoriteArrayOfUser);
     this.totalItems = this.favoriteProducts.size;
+  }
+
+  ngOnDestroy() {
+    if(this.userSubscription){
+      this.userSubscription.unsubscribe();
+    }
   }
 }
