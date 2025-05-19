a03c2c938846ac9d62f5350eeff8e3025be1232c
Juanquirr <juanquirr579@gmail.com>
2025-05-19 16:28:36 +0100

    Scroll update.

--- src/app/components/layouts/auth/auth.component.ts
+++ src/app/components/layouts/auth/auth.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import {RouterLink, RouterOutlet} from "@angular/router";
+import {IonContent} from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-layout-auth',
   standalone: true,
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss'],
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, IonContent],
 })
 export class AuthComponent {}
 
